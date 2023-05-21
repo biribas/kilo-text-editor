5ff942b6947c6d9ad0f9a6575b2a40aa511043ee
Carlos Adriano <cadrianogamac@gmail.com>
2023-05-20 22:46:23 -0300

    Reduces the wait time for reading characters to zero

--- src/terminal.c
+++ src/terminal.c
@@ -13,7 +13,7 @@ void enableRawMode(void) {
   raw.c_cflag |= CS8;
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+  raw.c_cc[VTIME] = 0;
 
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
     die("tcsetattr");
